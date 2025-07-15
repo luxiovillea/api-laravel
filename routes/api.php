f2a808663c6b1d9d9d10b73a064c97a40db7f3c9
LUXIOUSY\lucky <luckyputri01@gmail.com>
2025-07-15 15:17:05 +0700

    Refactor: Simplify analytics endpoint for stability

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnalyticsController;
 
@@ -8,14 +7,10 @@ use App\Http\Controllers\AnalyticsController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
+|
+| Endpoint ini akan mengambil data historis dari Google Analytics.
+| URL: /api/analytics/report/{propertyId}
+|
 */
 
-// HANYA ADA SATU RUTE INI:
-// URL /api/analytics-data akan memanggil fungsi fetchHistoricalData.
-Route::get('/analytics-data', [AnalyticsController::class, 'fetchHistoricalData']);
-
-
-// Rute default Laravel (biarkan saja)
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/analytics/report/{propertyId}', [AnalyticsController::class, 'fetchPageReport']);
